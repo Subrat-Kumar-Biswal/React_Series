1098b1186a7fc7d24e62c62e0646b0f4ce1635df
Subrat-Kumar-Biswal <subratkumar971@gmail.com>
2023-12-10 02:43:51 +0530

    react start

---
+++ 01_basic_react/src/App.js
@@ -0,0 +1,8 @@
+
+function App() {
+  return (
+    <h1>React Series Start | Subrat kumar biswal</h1>
+  );
+}
+
+export default App;
---
+++ 01_vite_react/src/App.jsx
@@ -0,0 +1,10 @@
+
+function App() {
+
+
+  return (
+    <h1> Start React With Vite</h1>
+  )
+}
+
+export default App
